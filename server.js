a6abaf762b2e9974b6690faf0f6644c2910e42c0
umer310 <umer.ataki@gmail.com>
2021-05-18 17:38:30 -0700

    add user in   db

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+connectDB();
+
+//Init Middleweare
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcom  to the Contact keeper APi" })
