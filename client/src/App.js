02e6189d41c50eb0e9a044d60c777cec742b7c5a
umer310 <umer.ataki@gmail.com>
2021-05-20 11:53:23 -0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import { About } from "./components/pages/About";
+import { Home } from "./components/pages/Home";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1> hello world</h1>
-    </div>
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 };
 
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <div className="my-1">this is a FULL</div>
+    </div>
+  );
+};
