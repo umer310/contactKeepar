d370379bb05c96e2c9a212e3c39f1cc849c161ed
umer310 <umer.ataki@gmail.com>
2021-05-21 20:06:27 -0700

    User AUTH is completed

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import AuthContext from "../../context/auth/authContext";
 import ContactFilter from "../contacts/ContactFilter";
 import ContactFrom from "../contacts/ContactFrom";
 import Contacts from "../contacts/Contacts";
 
 export const Home = () => {
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    authContext.loadUser();
+    //eslent-disable-next-line
+  }, []);
+
   return (
     <div className="grid-2">
       <div>
---
+++ client/src/context/alert/AlertState.js
@@ -0,0 +1,37 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import AlertContext from "./alertContext";
+import alertReducer from "./alertReducer";
+import { REMOVE_ALERT, SET_ALERT } from "../types";
+
+const AlertState = (props) => {
+  const initialState = [];
+
+  const [state, dispatch] = useReducer(alertReducer, initialState);
+
+  //set alert
+  const setAlert = (msg, type, timeout = 5000) => {
+    const id = uuid.v4();
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, type, id },
+    });
+
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+  };
+
+  //remover alert
+
+  return (
+    <AlertContext.Provider
+      value={{
+        alerts: state,
+        setAlert,
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
+  );
+};
+
+export default AlertState;
